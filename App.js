dea03b886253173173b54976e74723c4b4bd721a
Doyeon <duseh729@naver.com>
2023-06-01 21:09:39 +0900

    feat: feature-page header 컴포넌트 제작

--- App.js
+++ App.js
@@ -1,41 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import {React, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Header, Contents, Footer } from './components/Layout';
-import  {  FontAwesomeIcon  }  from  '@fortawesome/react-native-fontawesome';
+import { StatusBar } from "expo-status-bar";
+import { React, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Header, Contents, Footer } from "./components/Layout";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { FeatureHeader } from "./components/FeaturePage";
 
 export default function App() {
   const [defaultId, setDefaultId] = useState(1);
 
-    const idPlus = () => {
-      if (defaultId === 4) {
-        return setDefaultId(1);
-      }
-      setDefaultId(defaultId + 1);
-    };
-  
-    const idMinus = () => {
-      if (defaultId === 1) {
-        return setDefaultId(4);
-      }
-      setDefaultId(defaultId - 1);
-    };
+  const idPlus = () => {
+    if (defaultId === 4) {
+      return setDefaultId(1);
+    }
+    setDefaultId(defaultId + 1);
+  };
+
+  const idMinus = () => {
+    if (defaultId === 1) {
+      return setDefaultId(4);
+    }
+    setDefaultId(defaultId - 1);
+  };
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <Contents idPlus={idPlus} idMinus={idMinus} defaultId={defaultId}/>
-      <Footer />
+    <View>
+      <FeatureHeader param={"이용내역"} />
     </View>
-    
+
+    // <View style={styles.container}>
+    //   <Header />
+    //   <Contents idPlus={idPlus} idMinus={idMinus} defaultId={defaultId}/>
+    //   <Footer />
+    // </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
